dcb595578705eff924986ff40fb6dbbd431a2870
Pablo Márquez <pablomarquezartesano@gmail.com>
2023-09-12 09:19:00 +0200

    modelo para la api

---
+++ NumberApi/NumberApi.swift
@@ -0,0 +1,15 @@
+//
+//  NumberApi.swift
+//  NumberApi
+//
+//  Created by Pablo Márquez Marín on 8/9/23.
+//
+
+import Foundation
+
+struct NumberModel: Decodable {
+let text: String?
+let number: Int?
+let found: Bool?
+let type: String?
+}
